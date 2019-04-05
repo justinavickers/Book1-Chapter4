8cad5f9cc218dd7aa09dc3d1f786d53ea38ae4d9
justinavickers <justinav38@gmail.com>
2019-04-05 15:15:51 -0500

    planet list exercise complete

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>(){
+                "Mercury",
+                 "Mars"
+                 };
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> otherPlanets = new List<string>() {
+                "Uranus",
+                "Neptune"
+            };
+
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            planetList.AddRange(otherPlanets);
+            planetList.Add("Pluto");
+            planetList.Remove("Pluto");
+
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+            foreach ( string rocky in rockyPlanets) {
+                Console.WriteLine(rocky);
+            }
+
+            foreach( string planet in planetList ) {
+            Console.WriteLine(planet);
+            }
+        }
+    }
+}
